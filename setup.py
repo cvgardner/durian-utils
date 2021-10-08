4da88892ad37c783dc5589ab7b5606b4837c5d87
Chris Gardner <cgardner@alumni.caltech.edu>
2021-10-07 22:13:38 -0400

    Initialize package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='durian_utils',
+      version='0.1',
+      description='tools and wrappers used to create durian data projects',
+      url='http://github.com/storborg/funniest',
+      author='DurianIRL',
+      author_email='therealdurianirl@gmail.com',
+      license='MIT',
+      packages=['durian_utils'],
+      zip_safe=False)
